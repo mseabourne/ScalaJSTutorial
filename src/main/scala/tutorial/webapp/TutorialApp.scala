904b08c6a26534c75f3b886f5e0ff8a5e8dc0450
mseabourne <mseabourne@hotmail.com>
2015-10-26 23:15:36 -0700

    Fix problem with p tag compile error

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -4,17 +4,16 @@ import org.scalajs.dom;
 import dom.document;
 import scala.scalajs.js.JSApp;
 
-def appendPar(targetNode: dom.Node, text: String): Unit = {
-	val parNode = document.createElement("p")
-	val textNode = document.createTextNode(text)
-	parNode.appendChild(textNode)
-	targetNode.appendChild(parNode)
-}
-
 object TutorialApp extends JSApp {
 	def main() : Unit = {
 		appendPar(document.body, "Hello world via scala!");
 	}
-
+	
+	def appendPar(targetNode: dom.Node, text: String) : Unit = {
+		val parNode = document.createElement("p")
+		val textNode = document.createTextNode(text)
+		parNode.appendChild(textNode)
+		targetNode.appendChild(parNode)
+	}
 }
 
